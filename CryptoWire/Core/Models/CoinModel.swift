abbf171b4efbde8e4b6ccd902f278ca5bcaaafd6
Ravi <r.ranjanchn@gmail.com>
2022-05-01 00:31:15 +0530

    Refactored  foler struct

---
+++ CryptoWire/Core/Models/CoinModel.swift
@@ -0,0 +1,19 @@
+//
+//  CoinModel.swift
+//  CryptoWire
+//
+//  Created by Ravi Ranjan on 01/05/22.
+//
+
+import Foundation
+//
+/*
+ 
+ Coin Geco API Info :
+ 
+ API: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
+ */
+class Coin {
+    
+}
